24628c52c24d81a3d9fe2ced0d6645b23cbf3e1c
Anzu Futaba <fujisan.fujii@gmail.com>
2016-12-27 17:44:25 +0900

    まとも

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using Codeplex.Data;
 
@@ -9,7 +8,20 @@ namespace hapControlApp
 	class Program
 	{
         public static int nowVolume = 0;
-		static void Main(string[] args)
+        public static string nowPlaying = null;
+        public static string album = null;
+        public static string artist = null;
+        public static string codec = null;
+        public static string bandwidth = null;
+        public static double bitrate = 0;
+        public static string freq = null;
+        public static string uri = null;
+        public static double positionSec = 0;
+        public static int posSec = 0;
+        public static int posMin = 0;
+        public static string coverArtUrl = null;
+
+        static void Main(string[] args)
 		{
             Console.Title = "HAPControleApp";
             inputCmd();
@@ -26,68 +38,106 @@ namespace hapControlApp
 			HttpClient client = new HttpClient();
 			string setUrl = "http://192.168.0.2:60200/sony/" + url;
 			Uri Url = new Uri(setUrl);
-			client.DefaultRequestHeaders.Host = Url.Host;//ここから
-			client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
-			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
-			client.DefaultRequestHeaders.Add("Referer", "http://192.168.0.2:60100/HAP_app.html");
-			client.DefaultRequestHeaders.Add("Accept-Language", "gzip, deflate");
-			client.DefaultRequestHeaders.Add("Accept-Encoding", "ja,en-US;q=0.8,en;q=0.6");
-			client.DefaultRequestHeaders.Add("Origin", "http://192.168.0.2:60100");//ここまで要らない
 			client.DefaultRequestHeaders.ExpectContinue = false;//これは絶対いる
 			StringContent theContent = new StringContent(json, Encoding.UTF8, "application/x-www-form-urlencoded");
-			string result = client.PostAsync(Url, theContent).Result.Content.ReadAsStringAsync().Result;//送信はここ
+			dynamic result = client.PostAsync(Url, theContent).Result.Content.ReadAsStringAsync().Result;//送信部。帰ってくるJSONはresultに入る
 			Console.WriteLine(result);
-			if (isNeedParse == 1)//音量取り出し後に戻るやつ
-			{
-				string str = @"""volume"":";
-				int ind = result.IndexOf(str);
-				string volumeParam = result.Substring(ind,12);
-                int leng = volumeParam.Length;
-                string volume;
-                if (0 <= volumeParam.IndexOf(","))
-                {//,が含まれている=ボリュームが一桁
-                    volume = volumeParam.Substring(leng - 2,1);
-                    Console.WriteLine(volume);
-                }
-                else
-                {//含まれていない=ボリュームが二桁
-                    volume = volumeParam.Substring(leng - 2, 2);
-                    Console.WriteLine(volume);
-                }
-                int ivol = Int32.Parse(volume);
-                nowVolume = ivol;
+            dynamic data = DynamicJson.Parse(result);
+            if (isNeedParse == 1)//現在音量取得
+            {
+                nowVolume = int.Parse(data.result.volume);
             }
-			else
-			{
-				inputCmd();//parseが必要ない場合のみinputへ戻る
-			}
+            else if (isNeedParse == 2)//楽曲情報取得
+            {
+                nowPlaying = data.result[0].title;
+                album = data.result[0].albumName;
+                artist = data.result[0].artist;
+                codec = data.result[0].audioInfo[0].codec;
+                bandwidth = data.result[0].audioInfo[0].bandwidth;
+                bitrate = (double.Parse(data.result[0].audioInfo[0].bitrate)) / 1000;
+                freq = data.result[0].audioInfo[0].frequency;
+                coverArtUrl = data.result[0].coverArtUrl;
+                Console.WriteLine("曲名:" + nowPlaying);
+                Console.WriteLine("アルバム名;" + album);
+                Console.WriteLine("アーティスト:" + artist);
+                Console.WriteLine("コーデック:" + codec);
+                Console.WriteLine("ビットレート:" + bitrate + "kbps");
+                Console.WriteLine("サンプリング周波数:" + freq);
+                Console.WriteLine("ビット深度:" + bandwidth);
+
+                positionSec = data.result[0].positionSec;
+                posSec = (int)positionSec;
+                posMin = posSec / 60;//分数
+                posSec = posSec % 60;//秒数
+                Console.WriteLine(posMin + "分" + posSec + "秒");
+                Console.ReadLine();
+            }
+            else if (isNeedParse == 3)//ミュートチェック
+            {
+                if (data.result[0].mute == "on")
+                {
+                    makeMutejson(1);
+                }
+                if (data.result[0].mute == "off")
+                {
+                    makeMutejson(0);
+                }
+            }
+            else
+            {
+                inputCmd();//parseが必要ない場合のみinputへ戻る
+            }
 		}
 
-		static void serializeJson(dynamic obje,string url,int isNeedParse)
+        static void makeMutejson(int mode)//ミュート状態により処理変更
+        {
+            string str = null;
+            if (mode == 1)
+            {
+                str = "off";
+            }
+            else
+            {
+                str = "on";
+            }
+            var obj = new
+            {
+                @params = new[] {
+                        new {
+                            mute = str,
+                            }
+                    },
+                method = "setAudioMute",
+                version = "1.1",
+                id = 1,
+            };
+            serializeJson(obj, "audio", 0);
+        }
+
+		static void serializeJson(dynamic obje,string url,int isNeedParse)//シリアライズして次に流すだけのメソッド
 		{
 			var json = DynamicJson.Serialize(obje);
 			connectHap(json,url,isNeedParse);
 		}
 
-		static void setJunbi(string mode)
+		static void setJunbi(string mode)//コマンドの場合分け
 		{
 			string url = null;
-			if (mode == "previous")//前の曲へ
+			if (mode == "previous")//前の曲(頭出し)
 			{
-				var obj = new//JSON生成
+				var obj = new
 				{
 					@params = new[] { new { } },
 					method = "setPlayPreviousContent",
 					version = "1.0",
 					id = 1,
 				};
-
-				url = "avContent";
+   				url = "avContent";
 				serializeJson(obj,url,0);
 			}
 			if (mode == "poweron")//電源オン
 			{
-				var obj = new//JSON生成
+				var obj = new
 				{
 					@params = new[] {
 						new {
@@ -104,7 +154,7 @@ namespace hapControlApp
 			}
 			if (mode == "poweroff")//電源オフ
 			{
-				var obj = new//JSON生成
+				var obj = new
 				{
 					@params = new[] {
 						new {
@@ -119,9 +169,9 @@ namespace hapControlApp
 				url = "system";
 				serializeJson(obj,url,0);
 			}
-			if (mode == "start" || mode == "stop")//再生停止制御
+			if (mode == "start" || mode == "stop")//再生、停止
 			{
-				var obj = new//JSON生成
+				var obj = new
 				{
 					@params = new[] { new { } },
 					method = "pausePlayingContent",
@@ -131,9 +181,9 @@ namespace hapControlApp
 				url = "avContent";
 				serializeJson(obj,url,0);
 			}
-			if (mode == "next")//次の曲へ
+			if (mode == "next")//次の曲
 			{
-				var obj = new//JSON生成
+				var obj = new
 				{
 					@params = new[] { new { } },
 					method = "setPlayNextContent",
@@ -143,7 +193,7 @@ namespace hapControlApp
 				url = "avContent";
 				serializeJson(obj,url,0);
 			}
-			if (mode == "volumeup")
+			if (mode == "volumeup")//音量↑
 			{
                 dynamic getVolumeObj = getVolumeInfo();
                 url = "audio";
@@ -163,7 +213,7 @@ namespace hapControlApp
 				};
 				serializeJson(obj,url,0);
 			}
-			if (mode == "volumedown")
+			if (mode == "volumedown")//音量↓
 			{
                 dynamic getVolumeObj = getVolumeInfo();
                 url = "audio";
@@ -183,10 +233,33 @@ namespace hapControlApp
 				};
 				serializeJson(obj, url, 0);
 			}
+            if (mode == "getmusicinfo")//再生中楽曲情報取得
+            {
+                var obj = new
+                {
+                    @params = new[] {
+                        new {
+                            level ="detail",
+                        }
+                    },
+                    method = "getPlayingContentInfo",
+                    version = "1.2",
+                    id = 3,
+                };
+                url = "avContent";
+                serializeJson(obj, url, 2);
+            }
+            if (mode == "mute")//ミュートオンオフ
+            {
+                dynamic setMuteObj = getVolumeInfo();
+                url = "audio";
+                serializeJson(setMuteObj, url, 3);
+            }
+
 		}
 
-        static dynamic getVolumeInfo()
-        {//音量調節の下準備のための
+        static dynamic getVolumeInfo()//音量情報の取得用JSON生成
+        {
             var getVolumeObj = new
             {
                 @params = new[] { new { } },
@@ -196,17 +269,5 @@ namespace hapControlApp
             };
             return getVolumeObj;
         }
-	}
-
-	public class setJson
-	{
-		public string meth;
-		public string ver;
-		public int i;
-		public int param;
-		public string url;
-		public string paramStatus;
-		public string paramDetail;
-		public string paramVolume;
 	}
 }
